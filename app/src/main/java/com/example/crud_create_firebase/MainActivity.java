ae69b8a21554f2dc4f3aaaf9a865e71c35b23509
Braulio Laurelton <braulioraul22@hotmail.com>
2020-06-10 10:31:28 -0400

    Toast for each icon created to see what happen.

--- app/src/main/java/com/example/crud_create_firebase/MainActivity.java
+++ app/src/main/java/com/example/crud_create_firebase/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        //to put menu icons into the main menu.
         getMenuInflater().inflate(R.menu.menu_main,menu);
         return super.onCreateOptionsMenu(menu);
     }
@@ -24,6 +26,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         //switch
+
+        switch (item.getItemId()){
+            case R.id.icon_add:{
+                Toast.makeText(this, "Added", Toast.LENGTH_SHORT).show();
+            }
+            case R.id.icon_save:{
+                Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+            }
+            case R.id.icon_delete:{
+                Toast.makeText(this, "Deleted", Toast.LENGTH_SHORT).show();
+            }
+        }
         return true;
     }
 }
