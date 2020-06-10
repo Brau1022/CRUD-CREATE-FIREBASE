6bb9e896d3faa9916390f590078a52741dea6658
Braulio Laptop <braulioraul22@hotmail.com>
2020-06-09 20:41:44 -0400

    Update 1

---
+++ app/src/main/java/Model/Persona.java
@@ -0,0 +1,49 @@
+package Model;
+
+public class Persona {
+
+    public String Uid;
+    public String Nombre;
+    public String Apellido;
+    public String Correo;
+
+    public Persona() {
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return Nombre;
+    }
+}
